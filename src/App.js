013fbe44e06900672dca3018f94ce1ee9d1798db
Raunak Jodhawat <raunakjodhawat@gmail.com>
2020-06-19 19:43:45 -0400

    add correct github and linkedin links

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ export default class App extends React.Component {
         <h1>Sudoku Solver</h1>
         <Board />
         Made with <span style={{color: "#e25555"}}>&hearts;</span> &nbsp;[
-        <a href="https://github.com/raunakjodhawat/sudoku-frontend" >Github</a>, &nbsp;
-        <a href="https://github.com/raunakjodhawat/sudoku-frontend" >LinkedIn</a>]
+        <a href="https://github.com/raunakjodhawat/sudokuSolver" >Github</a>, &nbsp;
+        <a href="https://www.linkedin.com/in/jodhawat/" >LinkedIn</a>]
       </div>
     );
   }
